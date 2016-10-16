2441dc5251ccca45bb1f23b1ebab01b66cfa5878
VladMiryaha <vlad.miryaha@gmail.com>
2016-10-16 17:00:10 +0300

    fix it

--- dispersia_thread.c
+++ dispersia_thread.c
@@ -4,11 +4,23 @@
 #include <unistd.h>
 #include <math.h>
 #include <time.h>
+
+/*
+ * array ... not "massiv"
+ */
 #define MASS_SIZE 10000000
 #define THREAD_AMOUNT 4
 #define MOD 1000000
+
+/*
+ * лучше минимизировать использование глобальных переменных
+ */
 float number[MASS_SIZE];
 float average[THREAD_AMOUNT];
+
+/*
+ * dispersion
+ */
 float dispersia[THREAD_AMOUNT];
 struct Segment
 {
@@ -16,6 +28,11 @@ struct Segment
 	int end;
 	int index;
 };
+
+/*
+ * замените уже наконец символы табуляции на пробелы в настройках текстового редактора.
+ * т.к. они местами смешаны, то форматирование на других компьютерах выглядит съехавшим.
+ */
 
 void *mythread(void *dummy)
 {
@@ -26,8 +43,7 @@ void *mythread(void *dummy)
 	float sum = 0;
 	for (i = segment->begin; i < segment->end; i++)
 	{	
-		sum = sum + number[i];	
-			
+		sum = sum + number[i];
 	}
 	avg = sum / (segment->end - segment->begin);
 
@@ -52,15 +68,19 @@ int main(int argc, char const *argv[])
 	pthread_t thid[THREAD_AMOUNT]; 
 
 	int i = 0;
+  /*
+   * Почему вы только первую половину чисел инициализируете?
+   */
 	for (i = 0; i < MASS_SIZE / 2; i++)
 	{	
 		number[i] = (float)(rand() % MOD) / MOD;
-	
-		
 	}	
 
 	for (i = 0; i < THREAD_AMOUNT ; i++)
 	{
+    /*
+     * FIXIT: если у вас размер массива не делится нацело на количество нитей, то код ниже неверный.
+     */
 		segments[i].begin = (MASS_SIZE / THREAD_AMOUNT) * i;
 		segments[i].end = (MASS_SIZE / THREAD_AMOUNT) * (i + 1);
 		segments[i].index = i;
@@ -71,7 +91,6 @@ int main(int argc, char const *argv[])
 	for (i = 0; i < THREAD_AMOUNT ; i++)
 	{
 		result = pthread_create( &thid[i], (pthread_attr_t *)NULL, mythread, &segments[i]);
-	
 	}
 	
 	for (i = 0; i < THREAD_AMOUNT; ++i)
